ff5370fe5ff76c4bad99d1ebf09065527ff7ac6c
Ingun <ingun37@gmail.com>
2026-08-26 18:32:35 +0900

    Render Markdown (#2)

--- monorepo/apps/app/src/components/ChangeLogDialog.vue
+++ monorepo/apps/app/src/components/ChangeLogDialog.vue
@@ -1,7 +1,8 @@
 <template>
   <v-dialog v-model="isOpen" max-width="480">
     <v-card title="Change Log">
-      <v-card-text class="change-log">{{ changeLog }}</v-card-text>
+      <!-- eslint-disable-next-line vue/no-v-html -->
+      <v-card-text class="change-log" v-html="changeLogHtml" />
 
       <v-card-actions>
         <v-spacer />
@@ -12,14 +13,37 @@
 </template>
 
 <script lang="ts" setup>
+  import { marked } from 'marked'
   import changeLog from '../../CHANGELOG.md?raw'
 
   const isOpen = defineModel<boolean>({ required: true })
+
+  const changeLogHtml = marked.parse(changeLog, { async: false })
 </script>
 
 <style scoped>
-  .change-log {
-    white-space: pre-wrap;
-    font-family: monospace;
+  .change-log :deep(h1) {
+    font-size: 1.25rem;
+    margin-bottom: 0.5em;
+  }
+
+  .change-log :deep(h2) {
+    font-size: 1.1rem;
+    margin-top: 1em;
+    margin-bottom: 0.25em;
+  }
+
+  .change-log :deep(h3) {
+    font-size: 1rem;
+    margin-top: 0.5em;
+    margin-bottom: 0.25em;
+  }
+
+  .change-log :deep(ul) {
+    padding-left: 1.25em;
+  }
+
+  .change-log :deep(p) {
+    margin-bottom: 0.5em;
   }
 </style>
